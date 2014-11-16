57e48c435acb39eb3a39b4afdd4b86d79ea37817
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-11-16 11:48:35 +0900

    add include guard

--- mailbox.h
+++ mailbox.h
@@ -25,17 +25,22 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-int mbox_open();
-void mbox_close(int file_desc);
+#ifndef __MAILBOX_H_INCLUDED__
+#define __MAILBOX_H_INCLUDED__
 
-unsigned get_version(int file_desc);
-unsigned mem_alloc(int file_desc, unsigned size, unsigned align, unsigned flags);
-unsigned mem_free(int file_desc, unsigned handle);
-unsigned mem_lock(int file_desc, unsigned handle);
-unsigned mem_unlock(int file_desc, unsigned handle);
-void *mapmem(unsigned base, unsigned size);
-void *unmapmem(void *addr, unsigned size);
+   int mbox_open();
+   void mbox_close(int file_desc);
 
-unsigned execute_code(int file_desc, unsigned code, unsigned r0, unsigned r1, unsigned r2, unsigned r3, unsigned r4, unsigned r5);
-unsigned execute_qpu(int file_desc, unsigned num_qpus, unsigned control, unsigned noflush, unsigned timeout);
-unsigned qpu_enable(int file_desc, unsigned enable);
+   unsigned get_version(int file_desc);
+   unsigned mem_alloc(int file_desc, unsigned size, unsigned align, unsigned flags);
+   unsigned mem_free(int file_desc, unsigned handle);
+   unsigned mem_lock(int file_desc, unsigned handle);
+   unsigned mem_unlock(int file_desc, unsigned handle);
+   void *mapmem(unsigned base, unsigned size);
+   void *unmapmem(void *addr, unsigned size);
+
+   unsigned execute_code(int file_desc, unsigned code, unsigned r0, unsigned r1, unsigned r2, unsigned r3, unsigned r4, unsigned r5);
+   unsigned execute_qpu(int file_desc, unsigned num_qpus, unsigned control, unsigned noflush, unsigned timeout);
+   unsigned qpu_enable(int file_desc, unsigned enable);
+
+#endif /* __MAILBOX_H_INCLUDED__ */
