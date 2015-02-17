8fc01f4f21f0c9e5755ab797f76be261c2268a5e
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-02-17 16:47:28 +0900

    add error-free mailbox functions

---
+++ xmailbox.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include "mailbox.h"
+#include "error.h"
+
+void *xmapmem_cpu(unsigned base, unsigned size)
+{
+	return mapmem_cpu(base, size);
+}
+
+unsigned xmem_alloc(int file_desc, unsigned size, unsigned align, unsigned flags)
+{
+	unsigned handle = mem_alloc(file_desc, size, align, flags);
+	if (!handle) {
+		error("mem_alloc returned an error\n");
+		exit(EXIT_FAILURE);
+	}
+	return handle;
+}
+
+unsigned xmem_free(int file_desc, unsigned handle)
+{
+	unsigned handle = mem_free(file_desc, size, align, flags);
+	if (handle) {
+		error("mem_free returned an error\n");
+		exit(EXIT_FAILURE);
+	}
+	return handle;
+}
+
+unsigned xmem_lock(int file_desc, unsigned handle)
+{
+	unsigned p = mem_lock(file_desc, handle);
+	if (!handle) {
+		error("mem_lock returned an error\n");
+		exit(EXIT_FAILURE);
+	}
+	return p;
+}
+
+unsigned xmem_unlock(int file_desc, unsigned handle)
+{
+	unsigned p = mem_unlock(file_desc, handle);
+	if (!handle) {
+		error("mem_free returned an error\n");
+		exit(EXIT_FAILURE);
+	}
+	return handle;
+}
+
+int xmbox_open()
+{
+	return mbox_open();
+}
+
+void xmbox_close(int file_desc)
+{
+	mbox_close(file_desc);
+}
