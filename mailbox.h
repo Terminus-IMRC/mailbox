49e593560bfef65e5db3cfe3640f3e6b1b581dd3
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-03-31 18:00:14 +0900

    fix indent style

--- mailbox.h
+++ mailbox.h
@@ -29,19 +29,19 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef __MAILBOX_H_INCLUDED__
 #define __MAILBOX_H_INCLUDED__
 
-   int mbox_open();
-   void mbox_close(int file_desc);
+	int mbox_open();
+	void mbox_close(int file_desc);
 
-   unsigned get_version(int file_desc);
-   unsigned mem_alloc(int file_desc, unsigned size, unsigned align, unsigned flags);
-   unsigned mem_free(int file_desc, unsigned handle);
-   unsigned mem_lock(int file_desc, unsigned handle);
-   unsigned mem_unlock(int file_desc, unsigned handle);
-   void *mapmem_cpu(unsigned base, unsigned size);
-   void unmapmem_cpu(void *addr, unsigned size);
+	unsigned get_version(int file_desc);
+	unsigned mem_alloc(int file_desc, unsigned size, unsigned align, unsigned flags);
+	unsigned mem_free(int file_desc, unsigned handle);
+	unsigned mem_lock(int file_desc, unsigned handle);
+	unsigned mem_unlock(int file_desc, unsigned handle);
+	void *mapmem_cpu(unsigned base, unsigned size);
+	void unmapmem_cpu(void *addr, unsigned size);
 
-   unsigned execute_code(int file_desc, unsigned code, unsigned r0, unsigned r1, unsigned r2, unsigned r3, unsigned r4, unsigned r5);
-   unsigned execute_qpu(int file_desc, unsigned num_qpus, unsigned control, unsigned noflush, unsigned timeout);
-   unsigned qpu_enable(int file_desc, unsigned enable);
+	unsigned execute_code(int file_desc, unsigned code, unsigned r0, unsigned r1, unsigned r2, unsigned r3, unsigned r4, unsigned r5);
+	unsigned execute_qpu(int file_desc, unsigned num_qpus, unsigned control, unsigned noflush, unsigned timeout);
+	unsigned qpu_enable(int file_desc, unsigned enable);
 
 #endif /* __MAILBOX_H_INCLUDED__ */
