4ccc8e29fff9ed289f956977bf5ac40d58e8e535
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-06-21 12:16:15 +0900

    delete the declaration of xmapmem_cpu which should have deleted in 08a7882577a5457a41d261e54fbc85289057ffa8

--- mailbox.h
+++ mailbox.h
@@ -92,7 +92,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 	int xmbox_open();
 	void xmbox_close(int file_desc);
-	void *xmapmem_cpu(unsigned base, unsigned size);
 	unsigned xmem_alloc(int file_desc, unsigned size, unsigned align, unsigned flags);
 	unsigned xmem_free(int file_desc, unsigned handle);
 	unsigned xmem_lock(int file_desc, unsigned handle);
