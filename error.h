ff82c093f1aa09f1017c87e9b8f4f6bb59177631
Sugizaki Yukimasa <ysugi@idein.jp>
2018-03-24 20:04:45 +0900

    Remove old codes

--- error.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ERROR_H_INCLUDED_
-#define _ERROR_H_INCLUDED_
-
-#include <stdio.h>
-
-#define error(str, ...) fprintf(stderr, "%s:%d: error: "str, __FILE__, __LINE__, ## __VA_ARGS__)
-
-#endif /* _ERROR_H_INCLUDED_ */
